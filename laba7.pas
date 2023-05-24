ef83fcf34eeef1cb2c03b1e7d218f7d836443184
Pavel Alekseev <134404121+fiveightwo@users.noreply.github.com>
2023-05-24 07:15:31 +0300

    Add files via upload
    Some important files

---
+++ laba7.pas
@@ -0,0 +1,16 @@
+﻿program laba7;
+var S:string;
+i:integer;
+part:boolean;
+begin
+   writeln('Введите слово ');
+   readln(S);
+   part:=true;
+   for i:=1 to length(S) div 2 do
+     if S[i] <> S[length(S)-i+1] then
+     begin
+       part:=false;
+     end;
+     if part then write('Введенное слово является палиндромом')
+     else write('Введенное слово не является палиндромом');
+end.
